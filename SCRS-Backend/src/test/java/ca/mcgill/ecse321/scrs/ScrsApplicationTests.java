229d8fe7b12d792e2d9d7b2fa148eaf6c8eb3997
Adel Ahram <59708078+Invincidude@users.noreply.github.com>
2021-02-14 16:16:26 -0500

    Began Spring Backend Setup

---
+++ SCRS-Backend/src/test/java/ca/mcgill/ecse321/scrs/ScrsApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.scrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
