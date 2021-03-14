a5a5a3a1c1c0ecafbffd8082336087e17af4b055
Invincidude <adel.ahram2@gmail.com>
2021-03-14 18:26:10 -0400

    Updated DTO classes to better fit the model classes

--- SCRS-Backend/src/main/java/ca/mcgill/ecse321/scrs/dto/TechnicianDto.java
+++ SCRS-Backend/src/main/java/ca/mcgill/ecse321/scrs/dto/TechnicianDto.java
@@ -9,8 +9,6 @@ public class TechnicianDto
     private String name;
     private String email;
     private String phone;
-    private List<TimeslotDto> availabilities;
-
     public TechnicianDto()
     {
     }
@@ -18,26 +16,16 @@ public class TechnicianDto
     @SuppressWarnings("unchecked")
     public TechnicianDto(int id, String name)
     {
-        this(id, name, null, null, Collections.EMPTY_LIST);
+        this(id, name, null, null);
     }
 
-    @SuppressWarnings("unchecked")
-    public TechnicianDto(int id, String name, String email, String phone) {
-        this(id, name, email, phone, Collections.EMPTY_LIST);
-    }
-
-    public TechnicianDto(int id, String name, List<TimeslotDto> availabilityList)
-    {
-        this(id, name, null, null, availabilityList);
-    }
-
-    public TechnicianDto(int id, String name, String email, String phone, List<TimeslotDto> availabilityList)
+    public TechnicianDto(int id, String name, String email, String phone)
     {
         scrsUserId = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
-        availabilities = availabilityList;
+
     }
 
     public int getTechnicianId()
@@ -60,10 +48,6 @@ public class TechnicianDto
         return phone;
     }
 
-    public List<TimeslotDto> getTechnicianAvailabilities() {
-        return availabilities;
-    }
-
     public void setTechnicianEmail(String email)
     {
         this.email = email;
@@ -72,9 +56,5 @@ public class TechnicianDto
     public void setTechnicianPhone(String phone)
     {
         this.phone = phone;
-    }
-
-    public void setTechnicianAvailabilities(List<TimeslotDto> availabilityList) {
-        availabilities = availabilityList;
     }
 }
