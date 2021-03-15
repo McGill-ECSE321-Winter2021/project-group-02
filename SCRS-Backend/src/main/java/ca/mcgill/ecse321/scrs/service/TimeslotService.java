f3fef5982c7d47ad2b53203bc7fee1acc14c9f05
Roey Borsteinas <roeyb1@hotmail.com>
2021-03-15 17:29:07 -0400

    Implemented assignTechToTimeslot controller/service method

--- SCRS-Backend/src/main/java/ca/mcgill/ecse321/scrs/service/TimeslotService.java
+++ SCRS-Backend/src/main/java/ca/mcgill/ecse321/scrs/service/TimeslotService.java
@@ -1,6 +1,7 @@
 package ca.mcgill.ecse321.scrs.service;
 
 import ca.mcgill.ecse321.scrs.dao.TimeslotRepository;
+import ca.mcgill.ecse321.scrs.model.Technician;
 import ca.mcgill.ecse321.scrs.model.Timeslot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,14 @@ public class TimeslotService {
     @Transactional
     public List<Timeslot> getAllTimeslots() {
         return toList(timeslotRepository.findAll());
+    }
+
+    @Transactional
+    public void assignTechnicianToTimeslot(Technician tech, Timeslot ts) {
+        if (tech == null) throw new IllegalArgumentException("Invalid technician");
+        if (ts == null) throw new IllegalArgumentException("Invalid timeslot");
+
+        ts.addTechnician(tech);
+        timeslotRepository.save(ts);
     }
 }
