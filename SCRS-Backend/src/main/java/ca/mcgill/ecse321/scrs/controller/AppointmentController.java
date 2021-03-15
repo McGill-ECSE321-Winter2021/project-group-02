1d8aaa9c48ed1671e5524aeb148a3ca3301ea43a
Simon <simon.nakanemarcil@mail.mcgill.ca>
2021-03-15 16:39:36 -0400

    Updated Appointment Controller

--- SCRS-Backend/src/main/java/ca/mcgill/ecse321/scrs/controller/AppointmentController.java
+++ SCRS-Backend/src/main/java/ca/mcgill/ecse321/scrs/controller/AppointmentController.java
@@ -16,25 +16,30 @@ import static ca.mcgill.ecse321.scrs.controller.Helper.convertToDto;
 
 @RestController
 @RequestMapping(path = "/api/appointment", produces = MediaType.APPLICATION_JSON_VALUE)
-public class AppointmentController {
+public class AppointmentController
+{
     @Autowired
     AppointmentService appointmentService;
 
     @GetMapping("/getall/{id}")
-    public ResponseEntity<ArrayList<Appointment>> getAll(@PathVariable("id") String id) {
+    public ResponseEntity<ArrayList<Appointment>> getAll(@PathVariable("id") String id)
+    {
         int ID = Integer.valueOf(id);
         return new ResponseEntity<>(new ArrayList<Appointment>(), HttpStatus.OK);
     }
 
     @GetMapping("/notifications/{id}")
-    public ResponseEntity<ArrayList<Appointment>> notifications(@PathVariable("id") String id) {
+    public ResponseEntity<ArrayList<Appointment>> notifications(@PathVariable("id") String id)
+    {
         int ID = Integer.valueOf(id);
         return new ResponseEntity<>(new ArrayList<Appointment>(), HttpStatus.OK);
     }
 
-    @PostMapping(value = { "/book", "/book/" })
-    public AppointmentDto bookAppointment(@RequestBody Appointment appointment) {
-        if (appointment == null) {
+    @PostMapping(value = {"/book", "/book/"})
+    public AppointmentDto bookAppointment(@RequestBody Appointment appointment)
+    {
+        if (appointment == null)
+        {
             throw new IllegalArgumentException(
                     "Invalid appointment. Please submit a valid appointment booking to be created.");
         }
@@ -44,8 +49,9 @@ public class AppointmentController {
         return convertToDto(a);
     }
 
-    @PutMapping(value = { "/pay", "/pay/" })
-    public AppointmentDto payAppointment(@RequestParam(name = "appointmentId") int appointmentId) {
+    @PutMapping(value = {"/pay", "/pay/"})
+    public ResponseEntity<AppointmentDto> payAppointment(@RequestParam(name = "appointmentId") int appointmentId)
+    {
         Appointment appointment = appointmentService.getAppointmentById(appointmentId);
         appointment.setPaid(true);
         return new ResponseEntity<AppointmentDto>(convertToDto(appointment), HttpStatus.OK);
