17c2db322902f2bd86e11b8aee6f8277a13af9f5
Simon <simon.nakanemarcil@mail.mcgill.ca>
2021-03-16 01:25:10 -0400

    use case view available timeslot complete

--- SCRS-Backend/src/main/java/ca/mcgill/ecse321/scrs/controller/TimeslotController.java
+++ SCRS-Backend/src/main/java/ca/mcgill/ecse321/scrs/controller/TimeslotController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ca.mcgill.ecse321.scrs.controller.Helper.*;
 
+import java.sql.Date;
 import java.util.List;
 
 import static ca.mcgill.ecse321.scrs.controller.Helper.convertToDTO;
@@ -25,11 +26,18 @@ public class TimeslotController
     @Autowired
     TimeslotService timeslotService;
 
-    @GetMapping(value = {"/getServiceTimeslot", "/getServiceTimeslot/"})
-    public ResponseEntity<List<TimeslotDto>> getServiceTimeslot(@RequestParam(name = "appointmentType") AppointmentType type)
+    @GetMapping(value = {"/getTimeslots", "/getTimeslots/"})
+    public ResponseEntity<List<TimeslotDto>> getTimeslots()
     {
-        // TODO find available timeslots based on selected appointment type
-        return null;
+        List<Timeslot> timeslots = timeslotService.getAllTimeslots();
+        return new ResponseEntity<List<TimeslotDto>>(Helper.convertToDto(timeslots), HttpStatus.OK);
+    }
+
+    @GetMapping(value = {"/getAvailableTimeslot", "/getAvailableTimeslot/"})
+    public ResponseEntity<List<TimeslotDto>> getServiceTimeslot(@RequestParam(name = "startDate") Date startDate, @RequestParam(name = "endDate") Date endDate)
+    {
+        List<Timeslot> availableTimeslots = timeslotService.getAvailableTimeslots(startDate, endDate);
+        return new ResponseEntity<List<TimeslotDto>>(Helper.convertToDto(availableTimeslots), HttpStatus.OK);
     }
 
     @PostMapping(value = {"/assignTechTimeslot", "/assignTechTimeslot/"})
