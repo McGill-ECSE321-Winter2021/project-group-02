1863f21f7bfd552c6566d02954f3e1b5b7e03068
Invincidude <adel.ahram2@gmail.com>
2021-03-17 19:10:08 -0400

    Implemented TestAppointmentService Mockito Tests

--- SCRS-Backend/src/test/java/ca/mcgill/ecse321/scrs/service/TestSCRSService.java
+++ SCRS-Backend/src/test/java/ca/mcgill/ecse321/scrs/service/TestSCRSService.java
@@ -1,22 +1,7 @@
 package ca.mcgill.ecse321.scrs.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.lenient;
-
-import java.sql.Date;
-import java.sql.Time;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-
-import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.Answer;
 
 @ExtendWith(MockitoExtension.class)
 public class TestSCRSService
