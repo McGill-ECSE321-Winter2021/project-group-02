3ba38e992e79e032d28102f033017eb7c8c3b620
SgtMilk <alix.routhier-lalonde@hotmail.com>
2021-03-18 13:59:02 -0400

    added cookie code with uncomment TODOs

--- SCRS-Backend/src/main/java/ca/mcgill/ecse321/scrs/controller/CustomerController.java
+++ SCRS-Backend/src/main/java/ca/mcgill/ecse321/scrs/controller/CustomerController.java
@@ -27,12 +27,12 @@ public class CustomerController
         if (customer == null)
         {
             return new ResponseEntity<CustomerDto>(new CustomerDto(), HttpStatus.EXPECTATION_FAILED);
-            //throw new IllegalArgumentException("Invalid customer. Please submit a valid customer account to be created.");
+            // Invalid customer. Please submit a valid customer account to be created.
         }
         if (customerService.getCustomerByEmail(customer.getEmail()) != null)
         {
             return new ResponseEntity<CustomerDto>(new CustomerDto(), HttpStatus.ALREADY_REPORTED);
-            //throw new IllegalArgumentException("Email already in use, please try a different email address.");
+            // Email already in use, please try a different email address.
         }
         Customer newCustomer = customerService.createCustomer(customer.getEmail(), customer.getName(), hash(customer.getPassword()), customer.getPhone());
         return new ResponseEntity<>(convertToDto(newCustomer), HttpStatus.OK);
@@ -44,44 +44,52 @@ public class CustomerController
         int id = Integer.parseInt(ID);
         if (id == -1)
         {
-            //throw new IllegalArgumentException("Please login to modify a customer account.");
+            // TODO handle no login error with cookies (uncomment next line)
+            //return new ResponseEntity<CustomerDto>(new CustomerDto(), HttpStatus.UNAUTHORIZED);
+            // Please login to modify a customer account.
         }
         if (customer == null)
         {
             return new ResponseEntity<CustomerDto>(new CustomerDto(), HttpStatus.EXPECTATION_FAILED);
-            //throw new IllegalArgumentException("Invalid customer.");
+            // Invalid customer.
         }
         if (!isAdmin(scrsUserService.getSCRSUserByID(id)) && id != customer.getScrsUserId()) //does not have permission to edit.
         {
-            //throw new IllegalArgumentException("You cannot modify a customer account other than your own.");
+            // TODO handle bad login error with cookies (uncomment next line)
+            //return new ResponseEntity<CustomerDto>(new CustomerDto(), HttpStatus.UNAUTHORIZED);
+            // You cannot modify a customer account other than your own.
         }
         if (customerService.getCustomerByID(customer.getScrsUserId()) == null)
         {
             return new ResponseEntity<CustomerDto>(new CustomerDto(), HttpStatus.NOT_ACCEPTABLE);
-            //throw new IllegalArgumentException("No such customer found.");
+            // No such customer found.
         }
         Customer updatedCustomer = customerService.updateCustomerInfo(customer);
         return new ResponseEntity<>(convertToDto(updatedCustomer), HttpStatus.OK);
     }
 
-    @DeleteMapping(value = {"/delete", "/delete/"})
-    public ResponseEntity<CustomerDto> deleteCustomer(@RequestParam(value = "id") String customerIDString, @CookieValue(value = "id", defaultValue = "-1") String ID)
+    @DeleteMapping(value = {"/delete/{id}", "/delete/{id}/"})
+    public ResponseEntity<CustomerDto> deleteCustomer(@PathVariable String id, @CookieValue(value = "id", defaultValue = "-1") String ID)
     {
-        int customerID = Integer.parseInt(customerIDString);
-        int id = Integer.parseInt(ID);
-        if (id == -1)
+        int customerID = Integer.parseInt(id);
+        int idCookie = Integer.parseInt(ID);
+        if (idCookie == -1)
         {
-            //throw new IllegalArgumentException("Please login to delete a customer account.");
+            // TODO handle no login error with cookies (uncomment next line)
+            //return new ResponseEntity<CustomerDto>(new CustomerDto(), HttpStatus.UNAUTHORIZED);
+            // Please login to delete a customer account.
         }
         Customer customer = customerService.getCustomerByID(customerID);
         if (customer == null)
         {
             return new ResponseEntity<CustomerDto>(new CustomerDto(), HttpStatus.NOT_ACCEPTABLE);
-            //throw new IllegalArgumentException("Invalid customer. Please submit a valid customer account to be deleted.");
+            // Invalid customer. Please submit a valid customer account to be deleted.
         }
-        if (!isAdmin(scrsUserService.getSCRSUserByID(id)) && id != customerID) //does not have permission to edit.
+        if (!isAdmin(scrsUserService.getSCRSUserByID(idCookie)) && idCookie != customerID) //does not have permission to edit.
         {
-            // throw new IllegalArgumentException("You cannot delete a customer account other than your own.");
+            // TODO handle bad login error with cookies (uncomment next line)
+            //return new ResponseEntity<CustomerDto>(new CustomerDto(), HttpStatus.UNAUTHORIZED);
+            // You cannot delete a customer account other than your own.
         }
         return new ResponseEntity<>(convertToDto(customerService.deleteCustomer(customer)), HttpStatus.OK);
     }
