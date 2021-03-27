d8ffb65bb0717b5f7a19f1b48862d300ab5cf8ab
Invincidude <adel.ahram2@gmail.com>
2021-03-27 17:17:56 -0400

    Added CSS for myAccount page

--- client/src/pages/MyAccount.vue
+++ client/src/pages/MyAccount.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="myaccount">
 		<div id="myaccount-container">
+			<H1 id="myaccount-generic-title" class="myaccount-title"
+				>Edit Account Settings</H1
+			>
 			<div id="myaccount-edit">
 				<form
 					class="myaccount-form"
@@ -151,17 +154,107 @@
 			},
 		},
 		mounted() {
-			if (
-				document.cookie !== undefined &&
-				document.cookie !== -1 &&
-				document.cookie !== ""
-			) {
-				console.log(document.cookie);
-				this.$router.push("/dashboard");
-			}
-			//TODO if cookies eventually work, use the current cookie to get the user data to populate the form above
+			//TODO use vuex to get the current user id and data to populate the form above
 		},
 	};
 </script>
 
-<style scoped></style>
+<style scoped>
+	.myaccount {
+		height: 100vh;
+		width: 100vw;
+		background-image: radial-gradient(whitesmoke 20%, transparent 20%),
+			radial-gradient(rgb(235, 164, 89) 20%, transparent 20%);
+		background-position: 0 0, 7.5vh 7.5vh;
+		background-size: 15vh 15vh;
+		background-color: rgb(238, 207, 173);
+	}
+
+	#myaccount-container {
+		height: 90vh;
+		width: 50vw;
+		border-radius: 5vh;
+		background-color: whitesmoke;
+	}
+
+	.myaccount,
+	#myaccount-container,
+	.myaccount-form,
+	.myaccount-button-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		transition: 0.3s;
+	}
+
+	.myaccount-button {
+		all: unset;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 5vh;
+		width: 12vw;
+		border-radius: 2vh;
+		padding: 1vh;
+		background-color: rgb(235, 164, 89);
+		margin-top: 1vh;
+		margin-bottom: 1vh;
+		transition: 0.3s;
+		font-size: 3vh;
+		font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+		font-weight: 600;
+		color: rgb(75, 75, 75);
+		border: 0.5vh solid rgb(235, 164, 89);
+	}
+
+	.myaccount-button:hover {
+		background-color: rgb(175, 122, 65);
+		color: whitesmoke;
+		border-color: rgb(75, 75, 75);
+	}
+
+	.myaccount-input {
+		all: unset;
+		background-color: rgb(212, 211, 211);
+		font-size: 3vh;
+		width: 30vw;
+		margin-top: 1vh;
+		margin-bottom: 1vh;
+		padding: 2vh;
+		border-radius: 2vh;
+		font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+		font-weight: 600;
+		color: rgb(59, 58, 58);
+		transition: 0.3s;
+	}
+
+	.myaccount-input:focus {
+		background-color: rgb(175, 175, 175);
+	}
+
+	.myaccount-button-container {
+		flex-direction: row;
+	}
+
+	.myaccount-spacer {
+		width: 3vw;
+	}
+
+	#myaccount-edit-error {
+		opacity: 0;
+		font-size: 3vh;
+		font-weight: 600;
+		color: rgb(59, 58, 58);
+		transition: 0.3s;
+		font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+	}
+
+	.myaccount-title {
+		font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+		font-size: 3vh;
+		font-weight: 600;
+		color: rgb(59, 58, 58);
+		padding-bottom: 3vh;
+	}
+</style>
