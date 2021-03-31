f44b7d553b2f28b5673b1d516a31531cb92b330b
Simon <simon.nakanemarcil@mail.mcgill.ca>
2021-03-30 23:41:08 -0400

    Workspace page service calls

--- client/src/pages/WorkspacePage.vue
+++ client/src/pages/WorkspacePage.vue
@@ -27,7 +27,7 @@
            v-if="this.new === true"
       >
         <form class="form"
-              @submit.prevent="bookAppointment()"
+              @submit.prevent="createButton()"
         >
           <input class="text"
                  type="text"
@@ -35,7 +35,7 @@
                  v-model="newWorkspaceName"
           >
           <div>
-            <input type="button" class="button" value="Cancel">
+            <input type="button" class="button" value="Cancel" @click="cancelButton()">
             <input type="submit" class="button" value="Create">
           </div>
         </form>
@@ -45,20 +45,24 @@
            v-if="this.sel === true"
       >
         <form class="form"
-              @submit.prevent="bookAppointment()"
+              @submit.prevent="updateButton()"
         >
-          <input class="text"
+          <input id="edit-name-input" class="text"
                  type="text"
                  placeholder="new name"
                  v-model="newWorkspaceName"
           >
-          <input type="submit" class="button" value="Update">
+
+          <div>
+            <input type="button" class="button" value="Delete" @click="deleteButton()">
+            <input type="submit" class="button" value="Update">
+          </div>
         </form>
       </div>
 
-      <input id="return" class="button"
+      <input id="back" class="button"
              type="button"
-             value="Return"
+             value="Back"
              @click="backViewDash()"
       >
     </div>
@@ -66,15 +70,14 @@
 </template>
 
 <script>
-// import axios from "axios";
-// import proxy from "../constants.js";
+ import axios from "axios";
+ import proxy from "../constants.js";
 
 export default {
   name: "WorkspacePage",
   props: {
     msg: String,
   },
-  components: {},
   data() {
     return {
       curWorkspace: "Select a Workspace",
@@ -88,11 +91,10 @@ export default {
   methods: {
     backViewDash: function() {
       let t = this;
+      t.cancelButton();
       document.getElementById("workspace-title").style.opacity = 0;
       document.getElementById("workspace-option").style.opacity = 0;
-      document.getElementById("new-container").style.opacity = 0;
-      document.getElementById("edit-container").style.opacity = 0;
-      document.getElementById("return").style.opacity = 0;
+      document.getElementById("back").style.opacity = 0;
       setTimeout(function() {
         t.$router.push("/dashboard");
       }, 300);
@@ -105,15 +107,89 @@ export default {
       }
       this.sel = true;
       this.new = false;
+      this.newWorkspaceName = this.curWorkspace.workspaceName;
     },
+
     newButton: function () {
       if (this.new === true) return;
       this.new=true;
       this.sel=false;
       this.curWorkspace='Select a Workspace';
+      this.newWorkspaceName="";
+    },
+    cancelButton: function () {
+      this.new=false;
+      this.sel=false;
+      this.curWorkspace='Select a Workspace';
+      this.newWorkspaceName="";
+    },
+    createButton: async function () {
+      try {
+        let createWorkspaceData = "name=foo";
+
+        let createWorkspaceResponse = await axios.post(
+            proxy.proxy + "/api/workspace/create",
+            createWorkspaceData
+        );
+
+        if (createWorkspaceResponse.status === 200) {
+          this.workspaces.push(createWorkspaceResponse.data);
+          this.cancelButton();
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    updateButton: async function () {
+      try {
+        let updatedWorkspace = {
+          workspaceId: this.curWorkspace.workspaceId,
+          spaceName: this.newWorkspaceName
+        }
+
+        let updatedWorkspaceResponse = await  axios.put(
+            proxy.proxy + "/api/workspace/update",
+            updatedWorkspace
+        );
+
+        if (updatedWorkspaceResponse.status === 200) {
+          let index = this.workspaces.findIndex(workspace => workspace === this.curWorkspace);
+          this.workspaces[index] = updatedWorkspaceResponse.data;
+          this.curWorkspace = updatedWorkspaceResponse.data;
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    deleteButton: async function () {
+      try {
+        let workspaceId = this.curWorkspace.workspaceId;
+
+        let deleteWorkspaceResponse = await axios.delete(
+            proxy.proxy + `/api/workspace/delete/${workspaceId}`
+        );
+
+        if (deleteWorkspaceResponse.status === 200) {
+          this.cancelButton();
+        }
+      } catch (error) {
+        console.error(error);
+      }
     }
   },
   async mounted() {
+    //TODO UNCOMMENT ONCE BACKEND WORKS
+    /*if (this.$store.state.user !== "assistant") this.$router.push("/");
+
+    try {
+      let response = await axios.get(proxy.proxy + "/api/technician/getAll");
+      this.technicians = response.data;
+      response = await axios.get(proxy.proxy + "/api/workspace/getAll");
+      this.workspaces = response.data;
+    } catch (error) {
+      console.error(error);
+    }*/
+
     //TODO REMOVE DUMMY CODE BELOW ONCE BACKEND CONTAINS VALUES
     this.workspaces = [
       { workspaceName: "ws1", workspaceId: 4 },
@@ -132,10 +208,12 @@ export default {
     justify-content: center;
     flex-direction: column;
     margin-bottom: 1vh;
+    transition: 0.3s;
   }
 
   .h-container {
     margin-bottom: 1vh;
+    transition: 0.3s;
   }
 
   #workspace-page {
