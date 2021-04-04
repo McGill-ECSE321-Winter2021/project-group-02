485790f4955465555739c8957506520ccd1eb0e8
Invincidude <adel.ahram2@gmail.com>
2021-04-03 22:50:24 -0400

    Reformatted Front-end code

--- client/src/pages/Dashboard.vue
+++ client/src/pages/Dashboard.vue
@@ -1,182 +1,187 @@
 <template>
-  <div class="dashboard">
-    <button id="logout-button" @click="logout()">Logout</button>
-    <div
-      v-if="this.$store.state.userType === 'assistant'"
-      id="dashboard-container"
-    >
-      <div id="dashboard-change-opacity">
-        <AssistantDashboard />
-      </div>
-    </div>
-    <div
-      v-else-if="this.$store.state.userType === 'technician'"
-      id="dashboard-container"
-    >
-      <div id="dashboard-change-opacity">
-        <TechnicianDashboard />
-      </div>
-    </div>
-    <div v-else id="dashboard-container">
-      <div id="dashboard-change-opacity">
-        <CustomerDashboard />
-      </div>
-    </div>
-  </div>
+	<div class="dashboard">
+		<button id="logout-button" @click="logout()">Logout</button>
+		<div
+			v-if="this.$store.state.userType === 'assistant'"
+			id="dashboard-container"
+		>
+			<div id="dashboard-change-opacity">
+				<AssistantDashboard />
+			</div>
+		</div>
+		<div
+			v-else-if="this.$store.state.userType === 'technician'"
+			id="dashboard-container"
+		>
+			<div id="dashboard-change-opacity">
+				<TechnicianDashboard />
+			</div>
+		</div>
+		<div v-else id="dashboard-container">
+			<div id="dashboard-change-opacity">
+				<CustomerDashboard />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import AssistantDashboard from "./components/AssistantDashboard";
-import TechnicianDashboard from "./components/TechnicianDashboard";
-import CustomerDashboard from "./components/CustomerDashboard";
-import proxy from "../constants.js";
-import axios from "axios";
-import { mapActions } from "vuex";
+	import AssistantDashboard from "./components/AssistantDashboard";
+	import TechnicianDashboard from "./components/TechnicianDashboard";
+	import CustomerDashboard from "./components/CustomerDashboard";
+	import proxy from "../constants.js";
+	import axios from "axios";
+	import { mapActions } from "vuex";
 
-export default {
-  name: "Dashboard",
-  props: {
-    msg: String,
-  },
-  components: {
-    AssistantDashboard,
-    TechnicianDashboard,
-    CustomerDashboard,
-  },
-  data() {
-    return {};
-  },
-  methods: {
-    ...mapActions(["setUser"]),
-    logout: async function() {
-      try {
-        let logoutResponse = await axios.get(proxy.proxy + "/api/login/logout");
-        if (logoutResponse.status === 200) {
-          this.setUser({ user: -1, userType: "" });
-          document.getElementById("dashboard-change-opacity").style.opacity = 0;
-          document.getElementById("logout-button").style.opacity = 0;
-          document.getElementById("dashboard-container").style.height = "50vh";
-          document.getElementById("dashboard-container").style.width = "50vw";
-          let vm = this;
-          setTimeout(function() {
-            vm.$router.push("/");
-          }, 300);
-        } else return;
-      } catch (error) {
-        console.log(`${error}`);
-      }
-    },
-  },
-  mounted() {
-    if (this.$store.state.user === -1) this.$router.push("/");
-  },
-};
+	export default {
+		name: "Dashboard",
+		props: {
+			msg: String,
+		},
+		components: {
+			AssistantDashboard,
+			TechnicianDashboard,
+			CustomerDashboard,
+		},
+		data() {
+			return {};
+		},
+		methods: {
+			...mapActions(["setUser"]),
+			logout: async function() {
+				try {
+					let logoutResponse = await axios.get(
+						proxy.proxy + "/api/login/logout"
+					);
+					if (logoutResponse.status === 200) {
+						this.setUser({ user: -1, userType: "" });
+						document.getElementById(
+							"dashboard-change-opacity"
+						).style.opacity = 0;
+						document.getElementById("logout-button").style.opacity = 0;
+						document.getElementById("dashboard-container").style.height =
+							"50vh";
+						document.getElementById("dashboard-container").style.width = "50vw";
+						let vm = this;
+						setTimeout(function() {
+							vm.$router.push("/");
+						}, 300);
+					} else return;
+				} catch (error) {
+					console.log(`${error}`);
+				}
+			},
+		},
+		mounted() {
+			if (this.$store.state.user === -1) this.$router.push("/");
+		},
+	};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.dashboard {
-  height: 100vh;
-  width: 100vw;
-  background-image: radial-gradient(whitesmoke 20%, transparent 20%),
-    radial-gradient(rgb(235, 164, 89) 20%, transparent 20%);
-  background-position: 0 0, 7.5vh 7.5vh;
-  background-size: 15vh 15vh;
-  background-color: rgb(238, 207, 173);
-}
+	.dashboard {
+		height: 100vh;
+		width: 100vw;
+		background-image: radial-gradient(whitesmoke 20%, transparent 20%),
+			radial-gradient(rgb(235, 164, 89) 20%, transparent 20%);
+		background-position: 0 0, 7.5vh 7.5vh;
+		background-size: 15vh 15vh;
+		background-color: rgb(238, 207, 173);
+	}
 
-#logout-button {
-  all: unset;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 3vh;
-  width: 8vw;
-  border-radius: 2vh;
-  padding: 1vh;
-  background-color: rgb(182, 182, 182);
-  margin-top: 1vh;
-  margin-bottom: 1vh;
-  transition: 0.3s;
-  font-size: 3vh;
-  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
-  font-weight: 600;
-  color: rgb(75, 75, 75);
-  border: 0.5vh solid rgb(182, 182, 182);
-  text-align: center;
-  transition: 0.3s;
-  position: absolute;
-  top: 1vh;
-  right: 1vw;
-  animation: changeOpacity 0.3s;
-}
+	#logout-button {
+		all: unset;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 3vh;
+		width: 8vw;
+		border-radius: 2vh;
+		padding: 1vh;
+		background-color: rgb(182, 182, 182);
+		margin-top: 1vh;
+		margin-bottom: 1vh;
+		transition: 0.3s;
+		font-size: 3vh;
+		font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+		font-weight: 600;
+		color: rgb(75, 75, 75);
+		border: 0.5vh solid rgb(182, 182, 182);
+		text-align: center;
+		transition: 0.3s;
+		position: absolute;
+		top: 1vh;
+		right: 1vw;
+		animation: changeOpacity 0.3s;
+	}
 
-#logout-button:hover {
-  background-color: rgb(139, 139, 139);
-  color: whitesmoke;
-  border-color: rgb(75, 75, 75);
-}
+	#logout-button:hover {
+		background-color: rgb(139, 139, 139);
+		color: whitesmoke;
+		border-color: rgb(75, 75, 75);
+	}
 
-#dashboard-change-opacity {
-  animation: changeOpacity 0.3s;
-  transition: 0.3s;
-}
+	#dashboard-change-opacity {
+		animation: changeOpacity 0.3s;
+		transition: 0.3s;
+	}
 
-@keyframes changeOpacity {
-  from {
-    opacity: 0;
-  }
+	@keyframes changeOpacity {
+		from {
+			opacity: 0;
+		}
 
-  to {
-    opacity: 1;
-  }
-}
+		to {
+			opacity: 1;
+		}
+	}
 
-#dashboard-container {
-  height: 80vh;
-  width: 80vw;
-  border-radius: 5vh;
-  background-color: whitesmoke;
-  transition: 0.3s;
-}
+	#dashboard-container {
+		height: 80vh;
+		width: 80vw;
+		border-radius: 5vh;
+		background-color: whitesmoke;
+		transition: 0.3s;
+	}
 
-.dashboard,
-#dashboard-container,
-.dashboard-assistant-container,
-.dashboard-technician-container,
-.dashboard-customer-container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-}
+	.dashboard,
+	#dashboard-container,
+	.dashboard-assistant-container,
+	.dashboard-technician-container,
+	.dashboard-customer-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
 
-#a,
-#b,
-#c,
-#d {
-  border-radius: 5vh;
-  height: 40vh;
-  width: 40vw;
-}
+	#a,
+	#b,
+	#c,
+	#d {
+		border-radius: 5vh;
+		height: 40vh;
+		width: 40vw;
+	}
 
-#a {
-  grid-row: 1/2;
-  grid-column: 1/2;
-}
+	#a {
+		grid-row: 1/2;
+		grid-column: 1/2;
+	}
 
-#b {
-  grid-row: 1/2;
-  grid-column: 2/3;
-}
+	#b {
+		grid-row: 1/2;
+		grid-column: 2/3;
+	}
 
-#c {
-  grid-row: 2/3;
-  grid-column: 1/2;
-}
+	#c {
+		grid-row: 2/3;
+		grid-column: 1/2;
+	}
 
-#d {
-  grid-row: 2/3;
-  grid-column: 2/3;
-}
+	#d {
+		grid-row: 2/3;
+		grid-column: 2/3;
+	}
 </style>
--- client/src/pages/components/CustomerDashboard.vue
+++ client/src/pages/components/CustomerDashboard.vue
@@ -1,167 +1,164 @@
 <template>
-  <div class="customer-dashboard" id="customer-dashboard">
-    <div class="customer-dashboard-container" id="a">
-      <p class="dashboard-title">Appointments</p>
-      <div class="customer-dashboard-inside-container">
-        <button class="dashboard-button" @click="bookAppointment()">Create Appointment</button>
-        <div class="dashboard-spacer"></div>
-        <button class="dashboard-button">Modify Appointment</button>
-      </div>
-      <div class="customer-dashboard-inside-container">
+	<div class="customer-dashboard" id="customer-dashboard">
+		<div class="customer-dashboard-container" id="a">
+			<p class="dashboard-title">Appointments</p>
+			<div class="customer-dashboard-inside-container">
+				<button class="dashboard-button" @click="bookAppointment()">
+					Create Appointment
+				</button>
+				<div class="dashboard-spacer"></div>
+				<button class="dashboard-button">Modify Appointment</button>
+			</div>
+			<div class="customer-dashboard-inside-container">
+				<button class="dashboard-button" v-on:click="viewBookedAppt()">
+					View All Appointments
+				</button>
 
-        <button 
-          class="dashboard-button" 
-          v-on:click="viewBookedAppt()">View All Appointments
-        </button>
-
-        <div class="dashboard-spacer"></div>
-        <button class="dashboard-button"
-        v-on:click="viewNotifs()"
-        >View Notifications</button>
-      </div>
-      <div class="customer-dashboard-inside-container">
-        <button class="dashboard-button">
-          Available Time Slots
-        </button>
-        <div class="dashboard-spacer"></div>
-        <button class="dashboard-button">Pay for Service</button>
-      </div>
-      <div class="customer-dashboard-inside-container">
-        <button class="dashboard-button">Rate Service</button>
-      </div>
-    </div>
-    <div class="customer-dashboard-container" id="b">
-      <p class="dashboard-title">Accounts</p>
-      <button class="dashboard-button" @click="modifyAccount()">
-        Modify Account
-      </button>
-    </div>
-  </div>
+				<div class="dashboard-spacer"></div>
+				<button class="dashboard-button" v-on:click="viewNotifs()">
+					View Notifications
+				</button>
+			</div>
+			<div class="customer-dashboard-inside-container">
+				<button class="dashboard-button">
+					Available Time Slots
+				</button>
+				<div class="dashboard-spacer"></div>
+				<button class="dashboard-button">Pay for Service</button>
+			</div>
+			<div class="customer-dashboard-inside-container">
+				<button class="dashboard-button">Rate Service</button>
+			</div>
+		</div>
+		<div class="customer-dashboard-container" id="b">
+			<p class="dashboard-title">Accounts</p>
+			<button class="dashboard-button" @click="modifyAccount()">
+				Modify Account
+			</button>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "CustomerDashboard",
-  props: {
-    msg: String,
-  },
-  methods: {
-    modifyAccount: function() {
-      document.getElementById("customer-dashboard").style.opacity = 0;
-      document.getElementById("logout-button").style.opacity = 0;
-      let vm = this;
-      setTimeout(function() {
-        vm.$router.push("/myaccount");
-      }, 300);
-    },
-    bookAppointment: function() {
-      document.getElementById("customer-dashboard").style.opacity = 0;
-      let vm = this;
-      setTimeout(function() {
-        vm.$router.push("/bookappointment");
-      }, 300);
+	export default {
+		name: "CustomerDashboard",
+		props: {
+			msg: String,
+		},
+		methods: {
+			modifyAccount: function() {
+				document.getElementById("customer-dashboard").style.opacity = 0;
+				document.getElementById("logout-button").style.opacity = 0;
+				let vm = this;
+				setTimeout(function() {
+					vm.$router.push("/myaccount");
+				}, 300);
+			},
+			bookAppointment: function() {
+				document.getElementById("customer-dashboard").style.opacity = 0;
+				let vm = this;
+				setTimeout(function() {
+					vm.$router.push("/bookappointment");
+				}, 300);
+			},
+			viewBookedAppt: function() {
+				let t = this;
 
-    },
-    viewBookedAppt:function(){
-      let t = this;
+				setTimeout(function() {
+					t.$router.push("/viewbookedappointments");
+				}, 300);
+			},
 
-      setTimeout(function(){
-        t.$router.push("/viewbookedappointments");
-      }, 300);
+			viewNotifs: function() {
+				let t = this;
 
-    },
-
-    viewNotifs:function(){
-      let t = this;
-
-      setTimeout(function(){
-        t.$router.push("/viewnotifications");
-      }, 300);
-
-    },
-  },
-};
+				setTimeout(function() {
+					t.$router.push("/viewnotifications");
+				}, 300);
+			},
+		},
+	};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.customer-dashboard {
-  display: grid;
-  grid-template-columns: 40vw 40vw;
-  height: 80vh;
-  width: 80vw;
-  border-radius: 5vh;
-  background-color: whitesmoke;
-  transition: 0.3s;
-}
+	.customer-dashboard {
+		display: grid;
+		grid-template-columns: 40vw 40vw;
+		height: 80vh;
+		width: 80vw;
+		border-radius: 5vh;
+		background-color: whitesmoke;
+		transition: 0.3s;
+	}
 
-.customer-dashboard-container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-}
+	.customer-dashboard-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
 
-.customer-dashboard-inside-container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: row;
-}
+	.customer-dashboard-inside-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: row;
+	}
 
-#a,
-#b {
-  border-radius: 5vh;
-  height: 80vh;
-  width: 40vw;
-}
+	#a,
+	#b {
+		border-radius: 5vh;
+		height: 80vh;
+		width: 40vw;
+	}
 
-#a {
-  grid-row: 1/2;
-  grid-column: 1/2;
-}
+	#a {
+		grid-row: 1/2;
+		grid-column: 1/2;
+	}
 
-#b {
-  grid-row: 1/2;
-  grid-column: 2/3;
-}
+	#b {
+		grid-row: 1/2;
+		grid-column: 2/3;
+	}
 
-.dashboard-button {
-  all: unset;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 5vh;
-  width: 12vw;
-  border-radius: 2vh;
-  padding: 1vh;
-  background-color: rgb(235, 164, 89);
-  margin-top: 1vh;
-  margin-bottom: 1vh;
-  transition: 0.3s;
-  font-size: 3vh;
-  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
-  font-weight: 600;
-  color: rgb(75, 75, 75);
-  border: 0.5vh solid rgb(235, 164, 89);
-  text-align: center;
-}
+	.dashboard-button {
+		all: unset;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 5vh;
+		width: 12vw;
+		border-radius: 2vh;
+		padding: 1vh;
+		background-color: rgb(235, 164, 89);
+		margin-top: 1vh;
+		margin-bottom: 1vh;
+		transition: 0.3s;
+		font-size: 3vh;
+		font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+		font-weight: 600;
+		color: rgb(75, 75, 75);
+		border: 0.5vh solid rgb(235, 164, 89);
+		text-align: center;
+	}
 
-.dashboard-button:hover {
-  background-color: rgb(136, 93, 48);
-  color: whitesmoke;
-  border-color: rgb(75, 75, 75);
-}
+	.dashboard-button:hover {
+		background-color: rgb(136, 93, 48);
+		color: whitesmoke;
+		border-color: rgb(75, 75, 75);
+	}
 
-.dashboard-title {
-  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
-  font-size: 3vh;
-  font-weight: 600;
-  color: rgb(59, 58, 58);
-  padding-bottom: 5vh;
-}
+	.dashboard-title {
+		font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+		font-size: 3vh;
+		font-weight: 600;
+		color: rgb(59, 58, 58);
+		padding-bottom: 5vh;
+	}
 
-.dashboard-spacer {
-  width: 3vw;
-}
+	.dashboard-spacer {
+		width: 3vw;
+	}
 </style>
