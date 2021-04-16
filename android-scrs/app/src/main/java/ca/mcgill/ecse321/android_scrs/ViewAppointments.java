bb13a17d428df0295c0622326486ac20da71cce6
curious-duck <59633108+curious-duck@users.noreply.github.com>
2021-04-16 15:20:22 -0400

    Addition of the ViewAppointments Page (#97)
    * early draft
    
    * draft2
    
    * draft3 (restart) displays type and id as test
    
    * added switch
    
    * Displaying appointments (without decorations)
    
    * Tentative Final Version of ViewAppointments Page
    
    * clean up
    
    * Update strings.xml
    
    * Updated title text in book appointment page
    
    Co-authored-by: Invincidude <adel.ahram2@gmail.com>

---
+++ android-scrs/app/src/main/java/ca/mcgill/ecse321/android_scrs/ViewAppointments.java
@@ -0,0 +1,161 @@
+package ca.mcgill.ecse321.android_scrs;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+public class ViewAppointments extends Fragment {
+
+    List<Appointment> appointments;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        appointments = new ArrayList<Appointment>();
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.view_appointments, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_back_view_appointments).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ViewAppointments.this).navigate(R.id.action_ViewAppointments_to_dashboard);
+            }
+        });
+
+        AsyncHttpResponseHandler appointmentResponseHandler = new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                final String response = new String(responseBody);
+                try {
+
+                    final JSONArray jAppointmentList = new JSONArray(response);
+                    for (int i = 0; i < jAppointmentList.length(); ++i) {
+
+                        final JSONObject jAppointment = jAppointmentList.getJSONObject(i);
+
+                        final int appointmentId = jAppointment.getInt("appointmentId"); //Id used to get associated timeslot to know the date and time of the appointment
+                        String appointmentType = jAppointment.getString("appointmentType");
+                        String appointmentTypeString = "";
+
+                        switch (appointmentType) {
+                            case "CarWash":
+                                appointmentTypeString = "Car Wash";
+                                break;
+                            case "Maintenance":
+                                appointmentTypeString = "Maintenance";
+                                break;
+                            case "OilChange":
+                                appointmentTypeString = "Oil Change";
+                                break;
+                            case "TireChange":
+                                appointmentTypeString = "Tire Change";
+                                break;
+                            case "Towing":
+                                appointmentTypeString = "Towing";
+                                break;
+                            case "Inspection":
+                                appointmentTypeString = "Inspection";
+                                break;
+                            case "RoadsideAssistance":
+                                appointmentTypeString = "Roadside Assistance";
+                                break;
+                            case "Checkup":
+                                appointmentTypeString = "Checkup";
+                                break;
+                            default:
+                                appointmentTypeString = "Other";
+                                break;
+                        }
+
+                        String finalAppointmentTypeString = appointmentTypeString;
+                        AsyncHttpResponseHandler timeslotResponseHandler = new AsyncHttpResponseHandler() {
+
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                final String response = new String(responseBody);
+                                try {
+                                    final JSONObject jTimeslot = new JSONObject(response);
+
+                                    final String startDate = jTimeslot.getString("startDate");
+                                    final String endDate = jTimeslot.getString("endDate");
+                                    final String startTime = jTimeslot.getString("startTime");
+                                    final String endTime = jTimeslot.getString("endTime");
+
+                                    appointments.add(new Appointment(finalAppointmentTypeString, startDate, endDate, startTime, endTime, appointmentId));
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+
+                                RecyclerView appointmentView = view.findViewById(R.id.view_appointments_view);
+                                AppointmentAdapter adapter = new AppointmentAdapter(appointments);
+                                appointmentView.setAdapter(adapter);
+                                appointmentView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+                            }
+
+                            @Override
+                            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                            }
+                        };
+
+                        String url2 = Variables.getAbsoluteUrl("/api/appointment/getStartAndEnd/" + appointmentId);
+                        //Get a JSON Object containing the date and time of a given appointment
+                        Variables.client.get(ViewAppointments.super.getContext(), url2, null, "application/json", timeslotResponseHandler);
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                System.out.println(statusCode);
+            }
+
+        };
+
+        // Back Button Handling
+        view.findViewById(R.id.button_back_view_appointments).setOnClickListener(view1 -> {
+            if (Variables.userType.equals("customer")) {
+                NavHostFragment.findNavController(ViewAppointments.this)
+                        .navigate(R.id.action_ViewAppointments_to_dashboard);
+            }
+
+        });
+        String url = Variables.getAbsoluteUrl("/api/appointment/getall/" + Variables.userID); //gets all the appointments of a user
+        Variables.client.get(ViewAppointments.super.getContext(), url, null, "application/json", appointmentResponseHandler);
+
+
+    }
+
+}
+
