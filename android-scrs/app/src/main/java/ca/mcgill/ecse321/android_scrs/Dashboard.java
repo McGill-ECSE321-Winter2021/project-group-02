c5cac19f706e519e0bd45e5c18ca46d44f65acd8
curious-duck <59633108+curious-duck@users.noreply.github.com>
2021-04-16 02:31:58 -0400

    draft2

--- android-scrs/app/src/main/java/ca/mcgill/ecse321/android_scrs/Dashboard.java
+++ android-scrs/app/src/main/java/ca/mcgill/ecse321/android_scrs/Dashboard.java
@@ -54,14 +54,14 @@ public class Dashboard extends Fragment {
         view.findViewById(R.id.dash_view_appts_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                if(Variables.userType.equals("customer")){
-//                    NavHostFragment.findNavController(Dashboard.this).navigate(); TODO ADD ACTION WHEN BUTTON IS MAPPED
-//                } else{
-//                    Variables.userType = null;
-//                    Variables.userID = -1;
-//                    NavHostFragment.findNavController(Dashboard.this)
-//                            .navigate(R.id.action_dashboard_to_mainpage);
-//                }
+                if(Variables.userType.equals("customer")){
+                    NavHostFragment.findNavController(Dashboard.this).navigate(R.id.action_dashboard_to_viewAppointments);
+                } else{
+                    Variables.userType = null;
+                    Variables.userID = -1;
+                    NavHostFragment.findNavController(Dashboard.this)
+                            .navigate(R.id.action_dashboard_to_mainpage);
+                }
 
             }
         });
--- android-scrs/app/src/main/java/ca/mcgill/ecse321/android_scrs/ViewAppointments.java
+++ android-scrs/app/src/main/java/ca/mcgill/ecse321/android_scrs/ViewAppointments.java
@@ -8,16 +8,32 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 public class ViewAppointments extends Fragment {
 
+    List<Appointment> appointments;
     List<Timeslot> timeslots;
+    int appointmentId;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        appointments=new ArrayList<Appointment>();
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.view_appointments, container, false);
     }
@@ -25,46 +41,96 @@ public class ViewAppointments extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-            view.findViewById(R.id.view_technician_schedule_back).setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View view)
-                {
-                    NavHostFragment.findNavController(ViewAppointments.this).navigate(R.id.action_viewTechnicianSchedule_to_dashboard);
-                }
-            });
-
-            timeslots = new ArrayList<Timeslot>();
-            timeslots.add(new Timeslot("StartingTime", "Endtime", "", ""));
-            timeslots.add(new Timeslot("StartingTime", "Endtime", "", ""));
-            timeslots.add(new Timeslot("StartingTime", "Endtime", "", ""));
-            timeslots.add(new Timeslot("StartingTime", "Endtime", "", ""));
-            timeslots.add(new Timeslot("StartingTime", "Endtime", "", ""));
-            timeslots.add(new Timeslot("StartingTime", "Endtime", "", ""));
-            timeslots.add(new Timeslot("StartingTime", "Endtime", "", ""));
-            timeslots.add(new Timeslot("StartingTime", "Endtime", "", ""));
-
-            RecyclerView timeslotView = (RecyclerView)view.findViewById(R.id.view_technician_schedule_view);
-            TimeslotAdapter adapter = new TimeslotAdapter(timeslots);
-            timeslotView.setAdapter(adapter);
-            timeslotView.setLayoutManager(new LinearLayoutManager(view.getContext()));
-
-
-        // Back Button Handling
         view.findViewById(R.id.button_back_view_appointments).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (Variables.userType.equals("customer")) {
-                    NavHostFragment.findNavController(ViewAppointments.this)
-                            .navigate(R.id.action_login_to_login_Signup); //?????????????????
-                } else {
-                    Variables.userType = null;
-                    NavHostFragment.findNavController(ViewAppointments.this)
-                            .navigate(R.id.action_login_to_mainpage); //?????????
-                }
-
+                NavHostFragment.findNavController(ViewAppointments.this).navigate(R.id.action_ViewAppointments_to_dashboard);
             }
         });
 
+        AsyncHttpResponseHandler appointmentResponseHandler = new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                final String response = new String(responseBody);
+                try {
+                    final JSONArray jAppointmentList = new JSONArray(response);
+
+                    for (int i = 0; i < jAppointmentList.length(); ++i) {
+
+                        final JSONObject jAppointment = jAppointmentList.getJSONObject(i);
+
+                        appointmentId = jAppointment.getInt("appointmentId");
+                        final String appointmentType = jAppointment.getString("appointmentType");
+
+                        AsyncHttpResponseHandler timeslotResponseHandler = new AsyncHttpResponseHandler() {
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                final String response = new String(responseBody);
+                                try {
+                                    final JSONArray jTimeslotList = new JSONArray(response);
+                                    for (int i = 0; i < jTimeslotList.length(); ++i) {
+                                        final JSONObject jTimeslot = jTimeslotList.getJSONObject(i);
+                                        final String startDate = jTimeslot.getString("startDate");
+                                        final String endDate = jTimeslot.getString("endDate");
+                                        final String startTime = jTimeslot.getString("startTime");
+                                        final String endTime = jTimeslot.getString("endTime");
+
+                                        appointments.add(new Appointment(appointmentType,startDate, endDate, startTime, endTime,appointmentId));
+
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                                // Send another request to get all the workspaces
+                                String url = Variables.getAbsoluteUrl("/api/appointment/getStartAndEnd/"+ appointmentId);
+                                Variables.client.get(ViewAppointments.super.getContext(), url, null, "application/json", timeslotResponseHandler);
+                            }
+
+                            @Override
+                            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error)
+                            {
+                                System.out.println(statusCode);
+                            }
+                        };
+
+//                        appointment/getall/${this.$store.state.user}
+                        String url2 = Variables.getAbsoluteUrl("/api/appointment/getall/" + Variables.userID);
+                        Variables.client.get(ViewAppointments.super.getContext(), url2, null, "application/json", appointmentResponseHandler);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                RecyclerView appointmentView = view.findViewById(R.id.view_appointments_view);
+                AppointmentAdapter adapter = new AppointmentAdapter(appointments);
+                appointmentView.setAdapter(adapter);
+                appointmentView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error)
+            {
+                System.out.println(statusCode);
+            }
+
+        };
+
+
+        // Back Button Handling
+        view.findViewById(R.id.button_back_view_appointments).setOnClickListener(view1 -> {
+            if (Variables.userType.equals("customer")) {
+                NavHostFragment.findNavController(ViewAppointments.this)
+                        .navigate(R.id.action_ViewAppointments_to_dashboard);
+            }
+
+        });
+
+
     }
-}
+
+
+    }
+
